ec9c6decc07442fceaf340885f840f6d2e3ce52a
jill <guilmarlopez2007@example.com>
2026-03-26 17:43:41 +0800

    scrapped fabric for ignis. no proper documentation for fabric :<

--- fabric/.config/fabric/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-# modules/__init__.py
-
-from .bar import StatusBar
-#from .control_center import ControlCenter
-#from .notif_center import NotificationCenter
--- fabric/.config/fabric/services/__init__.py
+++
@@ -1,5 +0,0 @@
-# services/__init__.py
-
-from .sound import audioManager
-from .battery import batteryWidget
-from .internet import wifiWidget
